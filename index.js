8e187e217764cda4216f9464cab091bac2e08b25
daragmas <daragmas@gmail.com>
2022-07-12 13:49:03 -0400

    commit tree 0f6afc8d8af710f6b5f2794475bff7333029569e

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('')
+  if(word == reversedWord){return true}
+  else{return false}
 }
 
 /* 
